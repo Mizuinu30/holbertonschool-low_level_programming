0ad320967a01393c6404e648c635a544d5154742
Mizuinu30 <juansilva.dvm@gmail.com>
2023-06-15 17:58:06 +0000

    noprintf

---
+++ hello_world/4-puts.c
@@ -0,0 +1,18 @@
+/*
+ *
+ * file 4 puts.c
+ */
+
+#include <stdio.h>
+
+/*
+ * main prints ""Programming is like building a multilingual puzzle"
+ */
+
+int main(void)
+	{
+ 		puts("\"Programming is like building a multilingual puzzle");
+
+		return (0);
+
+	}
