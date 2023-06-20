af2e2bc4d6d4db10cba61a64db0eac051752fc1a
Mizuinu30 <juansilva.dvm@gmail.com>
2023-06-20 19:41:57 +0000

    nani?

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+/**
+ * main - prints all possible combinations
+ * of a single digit separated by a coma
+ * and followed by a space in ascending order.
+ *
+ * Return: Always prints (0).
+ */
+
+#include <stdio.h>
+
+int main()
+{
+    int i, j;
+
+    for (i = 0; i < 10; i++)
+	{
+        for (j = i; j < 10; j++)
+			{
+            putchar(i + '0');
+            putchar(j + '0');
+
+            if (j < 9) {
+                putchar(',');
+                putchar(' ');
+            }
+        }
+    }
+
+
+	putchar('\n');
+
+
+    return 0;
+}
+
