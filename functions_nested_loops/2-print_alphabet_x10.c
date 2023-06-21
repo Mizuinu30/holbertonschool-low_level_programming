3cdc7b2726ba5de37e5bc069c4b33821d1cb83c6
Mizuinu30 <juansilva.dvm@gmail.com>
2023-06-21 18:07:08 +0000

    alphax10

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times.
+ *
+ * Return: always (0).
+ */
+
+void print_alphabet_x10(void)
+{
+	int repetitions = 10, i;
+	char currentchar = 'a';
+
+	for (i = 0; i < repetitions; i++)
+	{
+		while (currentchar <= 'z')
+		{
+			_putchar(currentchar);
+			currentchar++;
+		}
+		currentchar = 'a';
+	}
+	_putchar('\n');
+
+}
+
+
