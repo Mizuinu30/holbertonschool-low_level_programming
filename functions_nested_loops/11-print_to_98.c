d5cdb15554e3192ad99fc462c08f6d9e6210e1a6
Mizuinu30 <juansilva.dvm@gmail.com>
2023-06-22 17:39:07 +0000

    count up

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * print_to_98 - prints all natural #s from n to 98.
+ * @n: # to strat to print from.
+ *
+ * Return: Always (0).
+ */
+
+void print_to_98(int n)
+{
+	for (; n <= 98; n++)
+	printf("%d, ", n);
+}
