422650e63e5f1f67388ed8adc67da04b3d6723a2
Mizuinu30 <juansilva.dvm@gmail.com>
2023-06-22 16:49:17 +0000

    sum

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds 2 integers.
+ * @num1: 1st number to verify.
+ * @num2: 2nd number to verify.
+ *
+ * Return: integer value.
+ */
+
+int add(int num1, int num2)
+{
+	return (num1 + num2);
+}
