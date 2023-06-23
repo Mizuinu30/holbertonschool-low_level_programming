ca11f0dfa85fd40cbe38611cd7c5e812c6c1685a
Mizuinu30 <juansilva.dvm@gmail.com>
2023-06-23 18:40:35 +0000

    printingnumbers

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_numbers - Write a function that prints the numbers,
+ *from 0 to 9, followed by a new line.
+ *
+ *Return(0)
+  */
+
+void print_numbers(void)
+{
+	char c = '0'
+
+	while (c <= '9')
+	{
+		_putchar(c);
+			c++;
+	}
+	_putchar('\n');
+}
