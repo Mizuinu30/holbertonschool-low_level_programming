1a687014b6d514dd82240776806b96d97d1c16ca
Mizuinu30 <juansilva.dvm@gmail.com>
2023-06-27 20:05:08 +0000

    jump

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,8 +8,9 @@
 void puts2(char *str)
 {
 	int index;
-	
-	for (index = 0; str[index] != '\0'; index++);
+
+	for (index = 0; str[index] != '\0'; index++)
+		_putchar(str[index]);
 
 	for (index = 0; str[index] != '\0'; index += 2)
 	{
