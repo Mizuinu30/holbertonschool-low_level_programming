7d5be7e04996317eb3ef8878b5d6b7fecdee652f
Mizuinu30 <juansilva.dvm@gmail.com>
2023-06-30 15:26:18 +0000

    set

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,10 +2,11 @@
 #include <stdio.h>
 
 /**
- * _memset - a function that fills the memory with a constant byte.
+ *_memset - a function that fills the memory with a constant byte.
  *@s:input var.
  *@b:char that will be passed.
  *@n: number of times b will be passed inside of s.
+ *Return: s.
  */
 
 char *_memset(char *s, char b, unsigned int n)
