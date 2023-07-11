bf443d46d8cba2a5670a388d1d83f3c2d265e572
Mizuinu30 <juansilva.dvm@gmail.com>
2023-07-11 19:56:08 +0000

    concat

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,30 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ *str_concat - pointer concatanated
+ *@s1: string 1
+ *@s2: string 2
+ *Return: nothing
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	int i, j;
+	char *ptr = malloc(sizeof(char) * strlen(s1) + strlen(s2) + 1);
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		ptr[i] = s1[i];
+	}
+	for (j = 0; s2[j] != '\0'; j++)
+	{
+		ptr[i++] = s2[j];
+	}
+	return (ptr);
+}
