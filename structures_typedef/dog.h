d0d69a88c13e2338cab87201ede8d0d58f825b29
Mizuinu30 <juansilva.dvm@gmail.com>
2023-07-17 13:45:47 +0000

    woof

---
+++ structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef dog_h
+#define dog_h
+
+/**
+ * struct dog - user define data type.
+ * @name: name char.
+ * @age: float.
+ * @owner: char name of owner.
+ */
+
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
