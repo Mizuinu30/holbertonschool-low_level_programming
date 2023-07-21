fb76d8996f1d37029a20fc4ce90e06e1d86ecd6e
Mizuinu30 <juansilva.dvm@gmail.com>
2023-07-21 18:53:24 +0000

    stringtheory

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,38 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_strings -  a function that prints strings, followed by a new line.
+ * @separator: string to be printed between strings.
+ * @n: number of strings passed to the function.
+ *
+ * Return: nothing.
+ */
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char *strg;
+
+	va_start(args, n);
+
+		for (i = 0; i < n; i++)
+		{
+			strg = va_arg(args,char *);
+			printf("%s", strg);
+
+			if (i < n - 1 && separator != NULL)
+			{
+			printf("%s", separator);
+			}
+			if (strg == NULL)
+			{
+			printf("%s", "nil");
+			}
+		}
+	va_end(args);
+
+	printf("\n");
+}
