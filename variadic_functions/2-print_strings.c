3eaeaf44449f4c34707899f0825aeea42011ed61
Mizuinu30 <juansilva.dvm@gmail.com>
2023-07-21 19:19:44 +0000

    hf

--- variadic_functions/2-print_strings.c
+++ variadic_functions/2-print_strings.c
@@ -24,7 +24,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 
 			if (strg == NULL)
 			{
-				printf("%s", "nil");
+				printf("%s", "(nil)");
 			}
 			else
 				printf("%s", strg);
