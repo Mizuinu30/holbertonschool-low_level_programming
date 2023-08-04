8ccba56499c949ec8ffe7c4a5f05207da4ce9f55
Mizuinu30 <juansilva.dvm@gmail.com>
2023-08-04 14:39:35 +0000

    length

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -1,9 +1,10 @@
 #include "lists.h"
 
 /**
- * list_len - a function that returns the number of 
+ * list_len - a function that returns the number of
  * elements in a linked list_t list.
  * @h: pointer to list.
+ * Return: count.
  */
 
 size_t list_len(const list_t *h)
